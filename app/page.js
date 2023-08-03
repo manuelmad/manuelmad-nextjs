f00384c20d202ec8f1869da9760215df7af34990
Manuel Araujo <manuelmad1981@gmail.com>
2023-08-03 10:17:58 -0400

    Iniciando proyecto Next JS

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          <Link href="/projects">Get</Link> started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
---
+++ app/projects/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Projects() {
+    return (
+        <>
+            <h1>Projects</h1>
+            <Link href="/">Back to home</Link>
+        </>
+    );
+}
