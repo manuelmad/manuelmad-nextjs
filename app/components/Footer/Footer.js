4ce1fe276f8bc5bcf68b855ea166d03c62d9610d
Manuel Araujo <manuelmad1981@gmail.com>
2024-01-29 19:47:04 -0400

    Fixing width on sections in projects page for wider monitors.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -6,35 +6,34 @@ export default function Header() {
     return (
         <footer id="contacto">
             <div className='sections-container'>
-            <section className="contact">
-                <div>
-                    <h1>Contact me</h1>
-                    <p>If you want to get in touch with me to know a bit more about my projects, collaborate in some of yours or offer me a job, this is the information you need.</p>
-                    <p id='link_container'>
-                        <Link href="/projects">MY PROJECTS</Link>
-                    </p>
-                </div>
-            </section>
-            <section className="contact-info">
-                <div>
-                    <h2>LinkedIn</h2>
-                    <p><a href="https://www.linkedin.com/in/manuel-araujo-beltran-994564153/" target="_blank">Manuel Araujo</a></p>
-                    <h2>YouTube</h2>
-                    <p><a href="https://www.youtube.com/channel/UCi6wdvohTO_5MuaIteiRm_A" target="_blank">@manuelaraujo81</a></p>
-                    <h2>GitHub</h2>
-                    <p><a href="https://github.com/manuelmad" target="_blank">manuelmad</a></p>
-                </div>
-            </section>
-            <section className="personal-logo">
-                <Image
-                    src="/imgs/logo-footer.png"
-                    alt="logo"
-                    width={241}
-                    height={241}
-                /> 
-            </section>
+                <section className="contact">
+                    <div>
+                        <h1>Contact me</h1>
+                        <p>If you want to get in touch with me to know a bit more about my projects, collaborate in some of yours or offer me a job, this is the information you need.</p>
+                        <p id='link_container'>
+                            <Link href="/projects">MY PROJECTS</Link>
+                        </p>
+                    </div>
+                </section>
+                <section className="contact-info">
+                    <div>
+                        <h2>LinkedIn</h2>
+                        <p><a href="https://www.linkedin.com/in/manuel-araujo-beltran-994564153/" target="_blank">Manuel Araujo</a></p>
+                        <h2>YouTube</h2>
+                        <p><a href="https://www.youtube.com/channel/UCi6wdvohTO_5MuaIteiRm_A" target="_blank">@manuelaraujo81</a></p>
+                        <h2>GitHub</h2>
+                        <p><a href="https://github.com/manuelmad" target="_blank">manuelmad</a></p>
+                    </div>
+                </section>
+                <section className="personal-logo">
+                    <Image
+                        src="/imgs/logo-footer.png"
+                        alt="logo"
+                        width={241}
+                        height={241}
+                    /> 
+                </section>
             </div>
-          
         </footer>
     );
 }
--- app/projects/page.js
+++ app/projects/page.js
@@ -28,6 +28,7 @@ export default function Projects() {
                 contact="CONTACT"
             />
             <main>
+                <section className="bg_container"></section>
                 <section className="introduction">
                     <div className="introduction-left">
                         <h1 className={work_sans.className}>My Projects</h1>
@@ -40,72 +41,97 @@ export default function Projects() {
                     </div>
                 </section>
                 <section className="casing-design justified-left">
-                    <h1>Casing Design.</h1>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/UClP-DTPBxA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                    <p>This web app allows to design every feature of the casings to be used in oil wells, which will cover the borehole in each phase of the drilling plan.</p>
-                    <p>Starting with the number of casing strings and their seating depths, including an adjustment based in critical conditions; following with the selection of diameters and connections of every pipe, going through the design of their nominal weight and steel grade, and finally representing all the results obtained in a schematic diagram.</p>
+                    <article>
+                        <h1>Casing Design.</h1>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/UClP-DTPBxA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        <p>This web app allows to design every feature of the casings to be used in oil wells, which will cover the borehole in each phase of the drilling plan.</p>
+                        <p>Starting with the number of casing strings and their seating depths, including an adjustment based in critical conditions; following with the selection of diameters and connections of every pipe, going through the design of their nominal weight and steel grade, and finally representing all the results obtained in a schematic diagram.</p>
+                    </article>
                 </section>
                 <section className="planetageek-webpage justified-right">
-                    <h1>Landing Page for Planeta Geek.</h1>
-                    <Image src="/imgs/planeta-geek-webpage.png" alt="imagen ejemplo de página web planeta geek" width={1893} height={892}/>
-                    <p>Planeta Geek is an excellent Venezuelan tv show based on videogames, tv series, movies, japanese animation and esports.</p>
-                    <p>I created this landing page as a recognition for their effort, and also give them the chance to have more resources to reach new people.</p>
-                    <p>The landging page includes a “web apps” section, where I included the links to some applications I’ve made to help them with the activities they perform for the public, including a Planeta Geek Videogame I developed with JS.</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg><a href="https://planetageek.netlify.app" target="_blank">GO TO APP</a>
-                    </p>
+                    <article>
+                        <h1>Landing Page for Planeta Geek.</h1>
+                        <div>
+                            <Image src="/imgs/planeta-geek-webpage.png" alt="imagen ejemplo de página web planeta geek" width={1893} height={892}/>
+                        </div>
+                        {/* <Image src="/imgs/planeta-geek-webpage.png" alt="imagen ejemplo de página web planeta geek" width={1893} height={892}/> */}
+                        <p>Planeta Geek is an excellent Venezuelan tv show based on videogames, tv series, movies, japanese animation and esports.</p>
+                        <p>I created this landing page as a recognition for their effort, and also give them the chance to have more resources to reach new people.</p>
+                        <p>The landging page includes a “web apps” section, where I included the links to some applications I've made to help them with the activities they perform for the public, including a Planeta Geek Videogame I developed with JS.</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg><a href="https://planetageek.netlify.app" target="_blank">GO TO APP</a>
+                        </p>
+                    </article>
                 </section>
                 <section className="videojuegos justified-left">
-                    <h1>Videogames App.</h1>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/eWY1OrTdAMY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                    <p>A single-page application.</p>
-                    <p>Consuming the API from RAWG (<a href="https://rawg.io/" target="_blank">rawg.io</a>) to create an inventory of the user’s games.</p>
-                    <p>The user can search a game by name and add it in 1 of the 3 available categories: played, playing and to play. Also can delete any game from any category.</p>
-                    <p>Clicking on the image of any game, a “details” view will be available.</p>
-                    <p>The app uses localstorage to preserve data.</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg><a href="https://manuelmad.github.io/API-REST/" target="_blank">GO TO APP</a>
-                    </p>
+                    <article>
+                        <h1>Videogames App.</h1>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/eWY1OrTdAMY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        <p>A single-page application.</p>
+                        <p>Consuming the API from RAWG (<a href="https://rawg.io/" target="_blank">rawg.io</a>) to create an inventory of the user's games.</p>
+                        <p>The user can search a game by name and add it in 1 of the 3 available categories: played, playing and to play. Also can delete any game from any category.</p>
+                        <p>Clicking on the image of any game, a “details” view will be available.</p>
+                        <p>The app uses localstorage to preserve data.</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg><a href="https://manuelmad.github.io/API-REST/" target="_blank">GO TO APP</a>
+                        </p>
+                    </article>
                 </section>
                 <section className="dolce-vita justified-right">
-                    <h1>Planeta Geek Videogame.</h1>
-                    <Image src="/imgs/videogame-pg-cover.png" width={1844} height={872} alt='Img planeta geek videogame'/>
-                    <p>I’ve created a short RPG videogame, with only 3 levels and a final scene.</p>
-                    <p>I put in practice some new concepts like: stablishing contact points of characters and objects as well as determining their collision, defining the actions due to those collisions, simultaneous render of several elements on the canvas, use of the setInterval () and setTimeout() functions, detection of the “key press” event, animations with sprites, etc.</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg><a href="https://planetageek-videogame.netlify.app/" target="_blank">GO TO APP</a>
-                    </p>
+                    <article>
+                        <h1>Planeta Geek Videogame.</h1>
+                        <div>
+                            <Image src="/imgs/videogame-pg-cover.png" width={1844} height={872} alt='Img planeta geek videogame'/>
+                        </div>
+                        <p>I've created a short RPG videogame, with only 3 levels and a final scene.</p>
+                        <p>I put in practice some new concepts like: stablishing contact points of characters and objects as well as determining their collision, defining the actions due to those collisions, simultaneous render of several elements on the canvas, use of the setInterval () and setTimeout() functions, detection of the “key press” event, animations with sprites, etc.</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg><a href="https://planetageek-videogame.netlify.app/" target="_blank">GO TO APP</a>
+                        </p>
+                    </article>
                 </section>
                 <section className="admin-condo justified-left">
-                    <h1>Receipts Generator.</h1>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/38yGQ5b1LVE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                    <p>I’ve created this web app so the administrator of a group of 100 houses can now generate customized receipts for the owners of each house by importing an Excel file and clicking a few buttons.</p>
-                    <p>He can then download any receipt in .pdf format, instead of creating each receipt individually (100 times) as he used to.</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg><a href="https://yacambu.netlify.app/" target="_blank">GO TO APP</a>
-                    </p>
+                    <article>
+                        <h1>Receipts Generator.</h1>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/38yGQ5b1LVE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        <p>I've created this web app so the administrator of a group of 100 houses can now generate customized receipts for the owners of each house by importing an Excel file and clicking a few buttons.</p>
+                        <p>He can then download any receipt in .pdf format, instead of creating each receipt individually (100 times) as he used to.</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#252C5A" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg><a href="https://yacambu.netlify.app/" target="_blank">GO TO APP</a>
+                        </p>
+                    </article>
                 </section>
                 <section className="lenin-caraballo justified-right">
-                    <h1>Budget Calculator for La Dolce Vita.</h1>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/pYeE2LPqQBg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                    <p>An application made with React.</p>
-                    <p>This web app is used by the owners of the cake shop “La Dolce Vita” to calculate budgets for their clients.</p>
+                    <article>
+                        <h1>Budget Calculator for La Dolce Vita.</h1>
+                        <div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/pYeE2LPqQBg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        </div>
+                        <p>An application made with React.</p>
+                        <p>This web app is used by the owners of the cake shop “La Dolce Vita” to calculate budgets for their clients.</p>
+                    </article>
                 </section>
                 <section className="formation-pressures justified-left">
-                    <h1>Pore and fracture pressures calculation.</h1>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/QVX95zT1mUo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                    <p>From seismic data, we can determine the pore (by using Pennebaker correlation) and fracture (by using Ben Eaton equation) pressures of the formations in the subsurface, before starting a well drilling.</p>
+                    <article>
+                        <h1>Pore and fracture pressures calculation.</h1>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/QVX95zT1mUo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        <p>From seismic data, we can determine the pore (by using Pennebaker correlation) and fracture (by using Ben Eaton equation) pressures of the formations in the subsurface, before starting a well drilling.</p>
+                    </article>
                 </section>
                 <section className="well-trajectory justified-right">
-                    <h1>Directional Wells Trajectory Design.</h1>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/JLMksGhN3VU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                    <p>This application takes the 6 values inputted by the user and calculates the needed parameters to graph the trajectory of a directional S shaped well.</p>
-                    <p>In the graph, it’s shown the TVD, MD and Vertical Section most important values of the trajectory.</p>
-                    <p>The app also has a “Warnings” section to show a message, describing a reason, in case the desired trajectory isn’t possible to perform.</p>
+                    <article>
+                        <h1>Directional Wells Trajectory Design.</h1>
+                        <div>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/JLMksGhN3VU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        </div>
+                        <p>This application takes the 6 values inputted by the user and calculates the needed parameters to graph the trajectory of a directional S shaped well.</p>
+                        <p>In the graph, it's shown the TVD, MD and Vertical Section most important values of the trajectory.</p>
+                        <p>The app also has a “Warnings” section to show a message, describing a reason, in case the desired trajectory isn't possible to perform.</p>
+                    </article>
                 </section>
             </main>
             <Footer/>
